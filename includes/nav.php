991e9f986664ad29ce2e67986d31a520019297c9
MartG31 <31081479+MartG31@users.noreply.github.com>
2019-08-07 11:28:17 +0200

    update MG

--- includes/nav.php
+++ includes/nav.php
@@ -1,32 +1,20 @@
-	<ul class="nav justify-content-center">
-	  <li class="nav-item">
+<ul class="nav justify-content-center">
 
-	  <?php if (isset($_SESSION['auth']) && $_SESSION['auth']): ?>
-	  		<li class="m-2 badge badge-success text-wrap">Bonjour  <br><?= $_SESSION['username']
-		  					; ?>					
-		  	</li>
+	<?php if (isset($_SESSION['auth']) && $_SESSION['auth']): ?>
 
-		  	<li class="nav-item">
-		    <a class="nav-link" href="index.php?page=goodbye"><button type="button" class="btn btn-danger">Quitter</button></a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="index.php?page=account"><button type="button" class="btn btn-primary">Mon compte</button></a>
-		  </li>
+	<li class="m-2 badge badge-success text-wrap">Bonjour <?= $_SESSION['username']; ?></li>
 
-		  <li class="m-2"> | </li>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=goodbye"><button type="button" class="btn btn-danger">Quitter</button></a></li>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=account"><button type="button" class="btn btn-primary">Mon compte</button></a></li>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=dashboard"><button type="button" class="btn btn-primary">Dashboard</button></a></li>
+	<li class="m-2"> | </li>
 
-	  <?php else: ?>
+	<?php else: ?>
 
-	  <li class="nav-item">
-	    <a class="nav-link" href="index.php?page=inscription"><button type="button" class="btn">Inscription</button></a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link" href="index.php?page=login"><button type="button" class="btn">Connexion</button></a>
-	  </li>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=inscription">Inscription</a></li>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=login"><button type="button" class="btn">Connexion</button></a></li>
 
 	<?php endif; ?>
 
-	<li class="nav-item">
-	    <a class="nav-link" href="index.php?page=contact"><button type="button" class="btn">Contact</button></a>
-	  </li>
-	</ul>
+	<li class="nav-item"><a class="nav-link" href="index.php?page=contact"><button type="button" class="btn">Contact</button></a></li>
+</ul>
--- index.php
+++ index.php
@@ -24,6 +24,7 @@ else { $page = "accueil"; }
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/dashboard.css">
 </head>
 <body>
     <div>
