1167c8559eeaec334ef0bd158f7d5922ec204a03
Mathieuwf3 <52195551+Mathieuwf3@users.noreply.github.com>
2019-08-08 14:11:05 +0200

    recup

--- pages/lost_password.php
+++ pages/lost_password.php
@@ -30,48 +30,59 @@ if(!empty($_POST)){
 
 	if(count($errors) === 0){
 		$token = bin2hex(random_bytes(50));
-		$mail = new PHPMailer;
-		$mail->SMTPOptions = ['ssl' => ['verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true]];
-		// $mail->SMTPDebug = 3; // mode debug si > 2
-		$mail->CharSet = 'UTF-8'; // charset utf-8
-		$mail->isSMTP(); // connexion directe à un serveur SMTP
-		$mail->isHTML(true); // mail au format HTML
-		$mail->Host = 'smtp.gmail.com'; // serveur SMTP
-		$mail->SMTPAuth = true; // serveur sécurisé
-		$mail->Port = 465; // port utilisé par le serveur
-		$mail->SMTPSecure = 'ssl'; // certificat SSL
-		$mail->Username = 'gsm3webforce3@gmail.com'; // login
-		$mail->Password = 'GSM3webforce3'; // mot de passe
-		$mail->AddAddress($safe['email']); // destinataire
-		// $mail->AddAddress('truc.muche@gmail.com'); // autre destinataire
-		// $mail->AddCC('machin@bidule.fr'); // copie carbone
-		// $mail->AddBCC('patron@societe.com'); // copie cachée
-		$mail->SetFrom('gsm3webforce3@gmail.com', 'GSM3'); // expéditeur
-		$mail->Subject = 'Message de GSM3'; // sujet
-		// le corps du mail au format HTML
-		$mail->Body = '<html>
-						<head>
-							<style>
-								h1{color: orange;}
-								h2{color: orange;}
-								p{color: orange;}
-								span{color:purple;text-decoration:none}
-							</style>
-						</head>
-						<body>
-							<h1>Bonjour,</h1>
-							<p>Vous avez indiqué que vous avez perdu votre mot de passe, veuillez cliquer sur le lien suivant pour récupérer l\'accès à votre compte.</p>
-							<p><a href="http://localhost/site_commu/password_recovery/reinit_password.php?token='.$token.'">Réinitialiser mon mot de passe</a></p>
-						</body>
-					</html>';
-		// envoi
-		$mail->Send();
-		$success = true;
-		echo $token;
-		// if($mail->Send()) {
-		// 	$success = true;
-		// 	echo $token;
-		// }
+		// Préparation de la requete
+		$request = $pdo->prepare('INSERT INTO password_resets (email, token) VALUES (:email, :token)');
+
+		// Association des paramètres avec les valeurs
+		// Les paramètres permettent de sécuriser les données
+		$paramsInsert = [
+			':email' => $safe['email'],
+			':token' => $token
+		];
+
+		// Let's go
+		if($request->execute($paramsInsert)) {
+			$success = true;
+			$mail = new PHPMailer;
+			$mail->SMTPOptions = ['ssl' => ['verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true]];
+			// $mail->SMTPDebug = 3; // mode debug si > 2
+			$mail->CharSet = 'UTF-8'; // charset utf-8
+			$mail->isSMTP(); // connexion directe à un serveur SMTP
+			$mail->isHTML(true); // mail au format HTML
+			$mail->Host = 'smtp.gmail.com'; // serveur SMTP
+			$mail->SMTPAuth = true; // serveur sécurisé
+			$mail->Port = 465; // port utilisé par le serveur
+			$mail->SMTPSecure = 'ssl'; // certificat SSL
+			$mail->Username = 'mathieu.webforce3@gmail.com'; // login
+			$mail->Password = 'AbC123456789'; // mot de passe
+			$mail->AddAddress($safe['email']); // destinataire
+			// $mail->AddAddress('truc.muche@gmail.com'); // autre destinataire
+			// $mail->AddCC('machin@bidule.fr'); // copie carbone
+			// $mail->AddBCC('patron@societe.com'); // copie cachée
+			$mail->SetFrom('mathieu.webforce3@gmail.com', 'GSM3'); // expéditeur
+			$mail->Subject = 'Message de GSM3'; // sujet
+			// le corps du mail au format HTML
+			$mail->Body = '<html>
+							<head>
+								<style>
+									h1{color: grey;}
+									p{color: grey;}
+								</style>
+							</head>
+							<body>
+								<h1>Bonjour,</h1>
+								<p>Vous avez indiqué que vous avez perdu votre mot de passe, veuillez cliquer sur le lien suivant pour récupérer l\'accès à votre compte.</p>
+								<p><a href="localhost/site_commu/pages/reinit_password.php?token='.$token.'">Réinitialiser mon mot de passe</a></p>
+							</body>
+						</html>';
+			// envoi
+			$mail->Send();
+			echo 'localhost/site_commu/pages/reinit_password.php?token='.$token;
+			// if($mail->Send()) {
+			// 	$success = true;
+			// 	echo $token;
+			// }
+		}
 	}
 }
 
